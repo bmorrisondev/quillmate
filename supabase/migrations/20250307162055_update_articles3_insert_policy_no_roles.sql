0196ff35f044df7485aa98293ba8b74e73ddf2cf
Brian Morrison II <brian@brianmorrison.me>
2025-03-26 16:18:56 -0500

    remove old migrations

--- supabase/migrations/20250307162055_update_articles3_insert_policy_no_roles.sql
+++
@@ -1,8 +0,0 @@
-drop policy if exists "Users can create articles" on public.articles3;
-
-create policy "Users can create articles"
-  on public.articles3
-  for insert
-  with check (
-    owner_id = requesting_owner_id()
-  );
